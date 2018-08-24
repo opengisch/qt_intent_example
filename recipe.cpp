674cec065ff612dbf1965b0237fd73f3d18c52e6
Marco Bernasocchi <marco@opengis.ch>
2018-08-24 16:32:01 +0300

    tests

--- recipe.cpp
+++ recipe.cpp
@@ -67,6 +67,40 @@ Recipe::Recipe(int id, const QString &title, int time, const QString &directions
 #if defined(Q_OS_ANDROID)
 void Recipe::createTimer(const QString &message) const
 {
+    QAndroidJniObject actionOpenDocument = QAndroidJniObject::getStaticObjectField( "android/content/Intent", "ACTION_OPEN_DOCUMENT", "Ljava/lang/String;" );
+      QAndroidJniObject categoryOpenable = QAndroidJniObject::getStaticObjectField( "android/content/Intent", "CATEGORY_OPENABLE", "Ljava/lang/String;" );
+      QAndroidJniObject intent = QAndroidJniObject( "android/content/Intent", "(Ljava/lang/String;)V", actionOpenDocument.object<jstring>() );
+      intent.callObjectMethod( "addCategory", "(Ljava/lang/String;)Landroid/content/Intent;", categoryOpenable.object<jstring>() );
+
+      // static void addFileType(String extension, int fileType, String mimeType
+
+      //QAndroidJniObject extension = QAndroidJniObject::fromString( QStringLiteral( "QGS" ) );
+      //jint fileType = 103;
+      QAndroidJniObject mimeType = QAndroidJniObject::fromString( QStringLiteral( "application/xml" ) );
+      //QAndroidJniObject uri = QAndroidJniObject::fromString( QStringLiteral( "file://*.qgs" ) );
+
+      //QAndroidJniObject parseString = QAndroidJniObject::fromString( QStringLiteral( "file://*.qgs" ) );
+
+      //QAndroidJniObject uri = QAndroidJniObject::callStaticObjectMethod("android/net/Uri",
+       //                                                                 "parse",
+        //                                                                "(Ljava/lang/String;)Landroid/net/Uri;",
+         //                                                               parseString.object<jstring>());
+
+      //QAndroidJniObject::callStaticObjectMethod("android/media/MediaFile", "addFileType", "(Ljava/lang/String;I;Ljava/lang/String;)V;",
+      //                                          extension.object<jstring>(),
+      //                                          fileType,
+      //                                          mimeType.object<jstring>());
+
+      //QAndroidJniObject mime = QAndroidJniObject::fromString( QStringLiteral( "application/qgs" ) );
+      intent.callObjectMethod( "setType", "(Ljava/lang/String;)Landroid/content/Intent;", mimeType.object<jstring>() );
+
+      //intent.setDataAndType(Uri.parse("file://" + getFilesDir() + "/abc.pdf"), "application/pdf");
+
+      //intent.callObjectMethod( "setDataAndType", "(Landroid/net/Uri;Ljava/lang/String;)Landroid/content/Intent;", uri.object<jobject>() , mimeType.object<jstring>() );
+      QtAndroid::startActivity( intent.object<jobject>(), 12);
+
+
+/*
     QAndroidJniObject ACTION_SET_TIMER = QAndroidJniObject::getStaticObjectField<jstring>("android/provider/AlarmClock",
                                                                                           "ACTION_SET_TIMER");
     QAndroidJniObject intent("android/content/Intent",
@@ -105,6 +139,8 @@ void Recipe::createTimer(const QString &message) const
         QtAndroid::startActivity(intent, 0);
     else
         qWarning() << "Unable to resolve activity";
+
+*/
 }
 #else
 void Recipe::createTimer() const
