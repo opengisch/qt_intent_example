4713c16ceb9978d273074d4306e8e4c65c647e70
Mario Baranzini <mario@opengis.ch>
2018-08-26 14:17:53 +0200

    Clean code

--- android/src/ch/opengis/qfield_recipe/QgsDocumentsProvider.java
+++ android/src/ch/opengis/qfield_recipe/QgsDocumentsProvider.java
@@ -84,7 +84,6 @@ public class QgsDocumentsProvider extends DocumentsProvider {
     // and search results.
     private static final int MAX_SEARCH_RESULTS = 20;
     private static final int MAX_LAST_MODIFIED = 5;
-
     private static final String ROOT = "root";
 
     // A file object at the root of the file hierarchy.  Depending on your implementation, the root
@@ -97,14 +96,9 @@ public class QgsDocumentsProvider extends DocumentsProvider {
         Log.v(TAG, "onCreate");
 
         mBaseDir = getContext().getFilesDir();
-
-
-        //writeDummyFilesToStorage();
-
         return true;
     }
 
-    // BEGIN_INCLUDE(query_roots)
     @Override
     public Cursor queryRoots(String[] projection) throws FileNotFoundException {
         Log.v(TAG, "queryRoots");
@@ -116,25 +110,16 @@ public class QgsDocumentsProvider extends DocumentsProvider {
 
         // If the user is not using QField, return an empty root cursor.  This removes our provider from
         // the list entirely.
+        // TODO
 
-
-         Log.v(TAG, "XXXXXXXXXXXXXXXXX"+getContext().getPackageName());
-        //Log.v(TAG, "************************************"+this.getReferrer().getHost());
         //if (!getContext().getPackageName().equals("ch.opengis.qfield")) {
-
-         //   Log.v(TAG, "inside the if");
-         //   return result;
+        //   return result;
         //}
 
         // It's possible to have multiple roots (e.g. for multiple accounts in the same app) -
         // just add multiple cursor rows.
         // Construct one row for a root called "MyCloud".
         final MatrixCursor.RowBuilder row = result.newRow();
-
-        //File root = Environment.getExternalStorageDirectory();
-        //String rootPath= root.getPath();
-
-        //scanFiles(new File(rootPath));
 
         row.add(Root.COLUMN_ROOT_ID, ROOT);
         row.add(Root.COLUMN_SUMMARY, getContext().getString(R.string.root_summary));
@@ -162,162 +147,31 @@ public class QgsDocumentsProvider extends DocumentsProvider {
 
         return result;
     }
-    // END_INCLUDE(query_roots)
 
-
-
-
-/*
-    // BEGIN_INCLUDE(query_recent_documents)
-    @Override
-    public Cursor queryRecentDocuments(String rootId, String[] projection)
-            throws FileNotFoundException {
-        //Log.v(TAG, "queryRecentDocuments");
-
-        // This example implementation walks a local file structure to find the most recently
-        // modified files.  Other implementations might include making a network call to query a
-        // server.
-
-        // Create a cursor with the requested projection, or the default projection.
-        final MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
-
-        final File parent = getFileForDocId(rootId);
-
-        // Create a queue to store the most recent documents, which orders by last modified.
-        PriorityQueue<File> lastModifiedFiles = new PriorityQueue<File>(5, new Comparator<File>() {
-            public int compare(File i, File j) {
-                return Long.compare(i.lastModified(), j.lastModified());
-            }
-        });
-
-        // Iterate through all files and directories in the file structure under the root.  If
-        // the file is more recent than the least recently modified, add it to the queue,
-        // limiting the number of results.
-        final LinkedList<File> pending = new LinkedList<File>();
-
-        // Start by adding the parent to the list of files to be processed
-        pending.add(parent);
-
-        // Do while we still have unexamined files
-        while (!pending.isEmpty()) {
-            // Take a file from the list of unprocessed files
-            final File file = pending.removeFirst();
-            if (file.isDirectory()) {
-                // If it's a directory, add all its children to the unprocessed list
-                Collections.addAll(pending, file.listFiles());
-            } else {
-                // If it's a file, add it to the ordered queue.
-                if(file.getPath().endsWith(".qgs")){
-                    lastModifiedFiles.add(file);
-                    }
-            }
-        }
-
-        // Add the most recent files to the cursor, not exceeding the max number of results.
-        for (int i = 0; i < Math.min(MAX_LAST_MODIFIED + 1, lastModifiedFiles.size()); i++) {
-            final File file = lastModifiedFiles.remove();
-            includeFile(result, null, file);
-        }
-        return result;
-    }
-    // END_INCLUDE(query_recent_documents)
-*/
-
-/*
-    // BEGIN_INCLUDE(query_search_documents)
-    @Override
-    public Cursor querySearchDocuments(String rootId, String query, String[] projection)
-            throws FileNotFoundException {
-        //Log.v(TAG, "querySearchDocuments");
-
-        // Create a cursor with the requested projection, or the default projection.
-        final MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
-        final File parent = getFileForDocId(rootId);
-
-        // This example implementation searches file names for the query and doesn't rank search
-        // results, so we can stop as soon as we find a sufficient number of matches.  Other
-        // implementations might use other data about files, rather than the file name, to
-        // produce a match; it might also require a network call to query a remote server.
-
-        // Iterate through all files in the file structure under the root until we reach the
-        // desired number of matches.
-        final LinkedList<File> pending = new LinkedList<File>();
-
-        // Start by adding the parent to the list of files to be processed
-        pending.add(parent);
-
-        // Do while we still have unexamined files, and fewer than the max search results
-        while (!pending.isEmpty() && result.getCount() < MAX_SEARCH_RESULTS) {
-            // Take a file from the list of unprocessed files
-            final File file = pending.removeFirst();
-            if (file.isDirectory()) {
-                // If it's a directory, add all its children to the unprocessed list
-                Collections.addAll(pending, file.listFiles());
-            } else {
-                // If it's a file and it matches, add it to the result cursor.
-                if (file.getName().toLowerCase().contains(query)) {
-                    includeFile(result, null, file);
-                }
-            }
-        }
-        return result;
-    }
-    // END_INCLUDE(query_search_documents)
-*/
-/*
-    // BEGIN_INCLUDE(open_document_thumbnail)
-    @Override
-    public AssetFileDescriptor openDocumentThumbnail(String documentId, Point sizeHint,
-                                                     CancellationSignal signal)
-            throws FileNotFoundException {
-        //Log.v(TAG, "openDocumentThumbnail");
-
-        final File file = getFileForDocId(documentId);
-        final ParcelFileDescriptor pfd =
-                ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
-        return new AssetFileDescriptor(pfd, 0, AssetFileDescriptor.UNKNOWN_LENGTH);
-    }
-    // END_INCLUDE(open_document_thumbnail)
-*/
-    // BEGIN_INCLUDE(query_document)
     @Override
     public Cursor queryDocument(String documentId, String[] projection)
             throws FileNotFoundException {
-        //Log.v(TAG, "queryDocument");
+        Log.v(TAG, "queryDocument");
 
         // Create a cursor with the requested projection, or the default projection.
         final MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
         includeFile(result, documentId, null);
         return result;
     }
-    // END_INCLUDE(query_document)
 
-
-    // BEGIN_INCLUDE(query_child_documents)
     @Override
     public Cursor queryChildDocuments(String parentDocumentId, String[] projection,
                                       String sortOrder) throws FileNotFoundException {
-        //Log.v(TAG, "queryChildDocuments, parentDocumentId: " +
-         //       parentDocumentId +
-          //      " sortOrder: " +
-          //      sortOrder);
 
+        Log.v(TAG, "queryDocument");
         final MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
-        //final File parent = getFileForDocId(parentDocumentId);
-        //for (File file : parent.listFiles()) {
-        //    includeFile(result, null, file);
-        //}
-
         File root = Environment.getExternalStorageDirectory();
         String rootPath= root.getPath();
 
         scanFiles(new File(rootPath), result);
 
-
         return result;
     }
-    // END_INCLUDE(query_child_documents)
-
 
     public void scanFiles(File file, MatrixCursor result) throws FileNotFoundException {
     File[] fileArray = file.listFiles();
@@ -331,8 +185,6 @@ public class QgsDocumentsProvider extends DocumentsProvider {
         }
     }
 
-
-    // BEGIN_INCLUDE(open_document)
     @Override
     public ParcelFileDescriptor openDocument(final String documentId, final String mode,
                                              CancellationSignal signal)
@@ -342,7 +194,6 @@ public class QgsDocumentsProvider extends DocumentsProvider {
         // periodically check the CancellationSignal.  If you have an extremely large file to
         // transfer from the network, a better solution may be pipes or sockets
         // (see ParcelFileDescriptor for helper methods).
-        Log.v(TAG, "%%%%%%%%%%%%" + documentId);
 
         final File file = getFileForDocId(documentId);
         final int accessMode = ParcelFileDescriptor.parseMode(mode);
@@ -350,50 +201,7 @@ public class QgsDocumentsProvider extends DocumentsProvider {
         return ParcelFileDescriptor.open(file, accessMode);
 
     }
-    // END_INCLUDE(open_document)
 
-/*
-    // BEGIN_INCLUDE(create_document)
-    @Override
-    public String createDocument(String documentId, String mimeType, String displayName)
-            throws FileNotFoundException {
-        //Log.v(TAG, "createDocument");
-
-        File parent = getFileForDocId(documentId);
-        File file = new File(parent.getPath(), displayName);
-        try {
-            file.createNewFile();
-            file.setWritable(true);
-            file.setReadable(true);
-        } catch (IOException e) {
-            throw new FileNotFoundException("Failed to create document with name " +
-                    displayName +" and documentId " + documentId);
-        }
-        return getDocIdForFile(file);
-    }
-    // END_INCLUDE(create_document)
-*/
-/*
-    // BEGIN_INCLUDE(delete_document)
-    @Override
-    public void deleteDocument(String documentId) throws FileNotFoundException {
-        //Log.v(TAG, "deleteDocument");
-        File file = getFileForDocId(documentId);
-        if (file.delete()) {
-            //Log.i(TAG, "Deleted file with id " + documentId);
-        } else {
-            throw new FileNotFoundException("Failed to delete document with id " + documentId);
-        }
-    }
-    // END_INCLUDE(delete_document)
-*/
-/*
-    @Override
-    public String getDocumentType(String documentId) throws FileNotFoundException {
-        File file = getFileForDocId(documentId);
-        return getTypeForFile(file);
-    }
-*/
     /**
      * @param projection the requested root column projection
      * @return either the requested root column projection, or the default projection if the
@@ -462,31 +270,14 @@ public class QgsDocumentsProvider extends DocumentsProvider {
     }
 
     /**
-     * Get the document ID given a File.  The document id must be consistent across time.  Other
+     * Get the document ID given a File.  The document id must be consistent across time. Other
      * applications may save the ID and use it to reference documents later.
-     * <p/>
-     * This implementation is specific to this demo.  It assumes only one root and is built
-     * directly from the file structure.  However, it is possible for a document to be a child of
-     * multiple directories (for example "android" and "images"), in which case the file must have
-     * the same consistent, unique document ID in both cases.
      *
      * @param file the File whose document ID you want
      * @return the corresponding document ID
      */
     private String getDocIdForFile(File file) {
         String path = file.getAbsolutePath();
-
-        // // Start at first char of path under root
-        // final String rootPath = mBaseDir.getPath();
-        // if (rootPath.equals(path)) {
-        //     path = "";
-        // } else if (rootPath.endsWith("/")) {
-        //     path = path.substring(rootPath.length());
-        // } else {
-        //     path = path.substring(rootPath.length() + 1);
-        // }
-
-        //return "root" + ':' + path;
         return path;
     }
 
@@ -548,99 +339,5 @@ public class QgsDocumentsProvider extends DocumentsProvider {
      */
     private File getFileForDocId(String docId) throws FileNotFoundException {
         return new File(docId);
-
-
-        // File target = mBaseDir;
-        // if (docId.equals(ROOT)) {
-        //     return target;
-        // }
-        // final int splitIndex = docId.indexOf(':', 1);
-        // if (splitIndex < 0) {
-        //     throw new FileNotFoundException("Missing root for " + docId);
-        // } else {
-        //     final String path = docId.substring(splitIndex + 1);
-        //     target = new File(target, path);
-        //     if (!target.exists()) {
-        //         throw new FileNotFoundException("Missing file for " + docId + " at " + target);
-        //     }
-        //     return target;
-        // }
     }
-
-
-    /**
-     * Preload sample files packaged in the apk into the internal storage directory.  This is a
-     * dummy function specific to this demo.  The MyCloud mock cloud service doesn't actually
-     * have a backend, so it simulates by reading content from the device's internal storage.
-     */
-    private void writeDummyFilesToStorage() {
-        if (mBaseDir.list().length > 0) {
-            return;
-        }
-
-        int[] imageResIds = getResourceIdArray(R.array.image_res_ids);
-        for (int resId : imageResIds) {
-            writeFileToInternalStorage(resId, ".jpeg");
-        }
-
-        int[] textResIds = getResourceIdArray(R.array.text_res_ids);
-        for (int resId : textResIds) {
-            writeFileToInternalStorage(resId, ".txt");
-        }
-
-        int[] docxResIds = getResourceIdArray(R.array.docx_res_ids);
-        for (int resId : docxResIds) {
-            writeFileToInternalStorage(resId, ".docx");
-        }
-    }
-
-    /**
-     * Write a file to internal storage.  Used to set up our dummy "cloud server".
-     *
-     * @param resId     the resource ID of the file to write to internal storage
-     * @param extension the file extension (ex. .png, .mp3)
-     */
-    private void writeFileToInternalStorage(int resId, String extension) {
-        InputStream ins = getContext().getResources().openRawResource(resId);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        int size;
-        byte[] buffer = new byte[1024];
-        try {
-            while ((size = ins.read(buffer, 0, 1024)) >= 0) {
-                outputStream.write(buffer, 0, size);
-            }
-            ins.close();
-            buffer = outputStream.toByteArray();
-            String filename = getContext().getResources().getResourceEntryName(resId) + extension;
-            FileOutputStream fos = getContext().openFileOutput(filename, Context.MODE_PRIVATE);
-            fos.write(buffer);
-            fos.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private int[] getResourceIdArray(int arrayResId) {
-        TypedArray ar = getContext().getResources().obtainTypedArray(arrayResId);
-        int len = ar.length();
-        int[] resIds = new int[len];
-        for (int i = 0; i < len; i++) {
-            resIds[i] = ar.getResourceId(i, 0);
-        }
-        ar.recycle();
-        return resIds;
-    }
-
-    /**
-     * Dummy function to determine whether the user is logged in.
-     */
-    private boolean isUserLoggedIn() {
-        final SharedPreferences sharedPreferences =
-                getContext().getSharedPreferences(getContext().getString(R.string.app_name),
-                        Context.MODE_PRIVATE);
-        return sharedPreferences.getBoolean(getContext().getString(R.string.key_logged_in), false);
-    }
-
-
-}
+ }
